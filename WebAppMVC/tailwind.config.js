c8672ff40f40b4b8e90cfb155267a6c66f486713
asafarim <asafarim.it@gmail.com>
2024-11-10 21:17:47 +0100

    style: Revamped Personal Data page layout using Tailwind CSS
    - Updated Personal Data page to a modern card layout using Tailwind CSS
    - Added responsive container with centered alignment for better UX
    - Improved styling for buttons: download and delete now have distinct colors
    - Enhanced text styling to make warnings more prominent and actions more visually clear

--- WebAppMVC/tailwind.config.js
+++ WebAppMVC/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Areas/**/*.cshtml', // Razor pages (if applicable) or MVC views (if applicable) or JavaScript files (if applicable)
-    './Pages/**/*.cshtml', // Razor pages
-    './Views/**/*.cshtml', // MVC views (if applicable)
-    './wwwroot/**/*.js',   // JavaScript files
+    "./WebAppMVC/**/*.cshtml", // Include all your Razor views
+    "./WebAppMVC/wwwroot/**/*.js", // Include JavaScript files that may use Tailwind classes
+    "./WebAppMVC/wwwroot/**/*.ts", // Include TypeScript files if applicable
   ],
   theme: {
     extend: {},
